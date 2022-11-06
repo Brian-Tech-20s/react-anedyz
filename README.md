59350f3559cacc6d0a6eb3b04862f0e3424f453b
Brian-Tech-20s <39637656+Brian-Tech-20s@users.noreply.github.com>
2022-11-06 18:08:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-anedyz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-anedyz)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+import Playbar from './Playbar'
+
+
+export default function App() {
+
+
+
+  
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <Playbar />
+
+
+    </div>
+  );
+}
